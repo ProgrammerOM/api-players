1b8943f88d85258922a7fb18ba5981489bf078fb
Zayden4159 <ddionn7@gmail.com>
2023-12-29 18:23:50 +0700

    NewUpdateNumber

--- controllers/PlayersOnline.js
+++ controllers/PlayersOnline.js
@@ -2,36 +2,36 @@ const Players = require("../Model/Players");
 
 async function PlayersOnline() {
   try {
-    let PlayersOnlineAdd = Math.floor(Math.random() * 10 + 1);
-    let PlayersOnlineReduce = Math.floor(Math.random() * 5 + 1);
-    let Playersonline = await Players.findOne({ title: "ผู้เล่นประจำวัน" });
-    console.log(`PlayersOnline ${Playersonline}`);
-    if (Playersonline != null) {
-      setInterval(async () => {
-        Playersonline.numbers += PlayersOnlineAdd;
+    setInterval(async () => {
+      let Playersonline = await Players.findOne({ title: "ผู้เล่นประจำวัน" });
+      console.log(`(ผู้เล่นประจำวัน)ค่าปัจจุบัน ${Playersonline}`);
+      if (Playersonline != null) {
+        function generateRandomIntegerInRange(min, max) {
+          return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+        Playersonline.numbers += generateRandomIntegerInRange(7, 15);
         let ShowPlayersOnlineAdd = await Players.findOneAndUpdate(
           { title: "ผู้เล่นประจำวัน" },
           { numbers: Playersonline.numbers }
         );
         console.log(`(ผู้เล่นประจำวัน)เพิ่มค่าขึ้น ${ShowPlayersOnlineAdd}`);
-
-        Playersonline.numbers -= PlayersOnlineReduce;
+        Playersonline.numbers -= generateRandomIntegerInRange(5, 15);
         let ShowPlayersOnlineReduce = await Players.findOneAndUpdate(
           { title: "ผู้เล่นประจำวัน" },
           { numbers: Playersonline.numbers }
         );
         console.log(`(ผู้เล่นประจำวัน)ลบค่าลง ${ShowPlayersOnlineReduce}`);
-      }, 5000);
-    } else {
-      await Players.findOneAndUpdate(
-        { title: "ผู้เล่นประจำวัน" },
-        { numbers: 20000 },
-        {
-          new: true,
-          upsert: true,
-        }
-      );
-    }
+      } else {
+        await Players.findOneAndUpdate(
+          { title: "ผู้เล่นประจำวัน" },
+          { numbers: 20000 },
+          {
+            new: true,
+            upsert: true,
+          }
+        );
+      }
+    }, 5000);
   } catch (error) {
     console.log(error);
   }
